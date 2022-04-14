b21a75c79108e31158ec749c7281d33707c87fe1
fazzaamiarso <fazzaamiarso@gmail.com>
2022-04-15 04:10:05 +0700

    Implement todo toggle check

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,7 +1,12 @@
 import type { Task } from "@prisma/client";
 import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
-import { Form, useLoaderData, useTransition } from "@remix-run/react";
+import {
+  Form,
+  useFetcher,
+  useLoaderData,
+  useTransition
+} from "@remix-run/react";
 import { useRef, useState, useEffect } from "react";
 import Timer from "~/component/Timer";
 import { db } from "~/utils/prisma.server";
@@ -15,6 +20,7 @@ export const action: ActionFunction = async ({ request }) => {
   const newTask = formData.get("task");
   const taskId = formData.get("taskId");
   const actionType = formData.get("action");
+  const isCompleted = Boolean(formData.get("isCompleted"));
 
   if (actionType && typeof actionType !== "string")
     return json({ message: "Action not allowed!" }, 405);
@@ -23,17 +29,23 @@ export const action: ActionFunction = async ({ request }) => {
   if (actionType === "delete" && taskId)
     return await db.task.delete({
       where: {
-        id: taskId,
-      },
+        id: taskId
+      }
     });
+  if (taskId) {
+    return await db.task.update({
+      where: { id: taskId },
+      data: { isCompleted }
+    });
+  }
 
   if (!newTask || typeof newTask !== "string")
     return json({ message: "Please insert a task!" }, 400);
 
   return await db.task.create({
     data: {
-      taskName: newTask,
-    },
+      taskName: newTask
+    }
   });
 };
 
@@ -51,13 +63,13 @@ export default function Index() {
   }, [transition]);
 
   return (
-    <div className="w-10/12 max-w-lg mx-auto py-4">
-      <h1 className="text-2xl font-bold text-red-700">
+    <div className='mx-auto w-10/12 max-w-lg py-4'>
+      <h1 className='text-2xl font-bold text-red-700'>
         Welcome to Remix Timer
       </h1>
       <button
         onClick={toggleBreak}
-        className="rounded-md py-2 bg-blue-500 font-bold"
+        className='rounded-md bg-blue-500 py-2 font-bold'
       >
         {isBreak ? "Study" : "Break"}
       </button>
@@ -66,53 +78,84 @@ export default function Index() {
       ) : (
         <Timer key={Date.now()} initialTime={25} />
       )}
-      <div className="mt-8">
-        <ul className="space-y-4 pb-4`">
+      <div className='mt-8'>
+        <ul className='pb-4` space-y-4'>
           {tasks.length ? (
             tasks.map((task) => {
               return (
-                <li key={task.id} className="flex gap-2">
-                  {task.taskName}
-                  <Form method="post">
-                    <input
-                      type="text"
-                      hidden
-                      name="taskId"
-                      defaultValue={task.id}
-                    />
-                    <button
-                      className="bg-red-600 px-2 text-white rounded-sm"
-                      name="action"
-                      value="delete"
-                      type="submit"
-                    >
-                      Delete
-                    </button>
-                  </Form>
-                </li>
+                <ListItem
+                  key={task.id}
+                  id={task.id}
+                  taskName={task.taskName}
+                  isCompleted={task.isCompleted}
+                />
               );
             })
           ) : (
-            <p className="text-lg">No task yet!</p>
+            <p className='text-lg'>No task yet!</p>
           )}
         </ul>
-        <Form method="post" className="flex gap-2">
+        <Form method='post' className='flex gap-2'>
           <input
             ref={inputRef}
-            type="text"
-            id="task"
-            name="task"
-            aria-label="task"
+            type='text'
+            id='task'
+            name='task'
+            aria-label='task'
             required
           />
           <button
-            type="submit"
-            className="bg-pink-600 text-white font-semibold px-2"
+            type='submit'
+            className='bg-pink-600 px-2 font-semibold text-white'
           >
             Add task
           </button>
         </Form>
       </div>
     </div>
+  );
+}
+
+type ListItemProps = {
+  id: string;
+  taskName: string;
+  isCompleted: boolean;
+};
+function ListItem({ id, taskName, isCompleted }: ListItemProps) {
+  const itemFetcher = useFetcher();
+
+  return (
+    <li key={id}>
+      <itemFetcher.Form
+        method='post'
+        className='flex gap-2'
+        onChange={(e) => {
+          itemFetcher.submit(e.currentTarget, {
+            replace: true
+          });
+        }}
+      >
+        <input
+          type='checkbox'
+          id={id}
+          name='isCompleted'
+          defaultChecked={isCompleted}
+        />
+        <label htmlFor={id}>{taskName}</label>
+        <input type='text' hidden name='taskId' defaultValue={id} />
+        <button
+          className={`rounded-sm bg-red-600 px-2 text-white ${
+            itemFetcher.submission?.formData.get("action") === "delete"
+              ? "opacity-60"
+              : ""
+          }`}
+          name='action'
+          value='delete'
+          type='submit'
+        >
+          Delete
+        </button>
+      </itemFetcher.Form>
+    </li>
   );
 }
